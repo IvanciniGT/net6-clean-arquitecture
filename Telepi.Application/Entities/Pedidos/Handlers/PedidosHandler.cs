3003b15c74ea7fec2fa3915a768cbf59c947cddf
Ivan Osuna Ayuste <ivan.osuna.ayuste@gmail.com>
2022-10-25 14:39:02 +0200

    dia 5

--- Telepi.Application/Entities/Pedidos/Handlers/PedidosHandler.cs
+++ Telepi.Application/Entities/Pedidos/Handlers/PedidosHandler.cs
@@ -6,26 +6,30 @@ using Telepi.Business.ValueObjects;
 
 namespace Telepi.Application.Entities.Pedidos.Handlers;
 
-public class PedidosHandler
+public class PedidosHandler : ICommandHandler
 {
 
     private IRepositorio repositorio;
     private IMediador mediador;
+    private IMediadorComandos mediadorComandos;
 
-    public PedidosHandler(IRepositorio repositorio, using Telepi.Application.Entities.Pedidos.Commands;
- )
+    public PedidosHandler(IRepositorio repositorio, IMediador mediador, IMediadorComandos mediadorComandos)
     {
         this.repositorio = repositorio;
         this.mediador = mediador;
+        this.mediadorComandos = mediadorComandos;
+        this.mediadorComandos.subscribe( this );
     }
 
-    internal nuevoPedido(NuevoPedidoCommand comando) {
+
+    internal void handle(NuevoPedidoCommand comando) {
         IReadOnlyCollection<PizzaPersonalizada> pizzasAIncluir;// = comando.pizzas; Lo podré hacer con un mapper
         // El problema de usar un automapper es que convierto el objeto en mutable
         // Si monto yo el mapeo... me quito de ese problema
         Pedido nuevoPedido = new Pedido(IMediador mediador, comando.cliente, pizzasAIncluir);
+        int id=repositorio.persistir(nuevoPedido);
+        nuevoPedido.Id = id;
 
     }
 
 }
-
